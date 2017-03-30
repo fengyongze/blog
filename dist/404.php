224d97de255856f9e3542fea3df4c95ac9566fe2
Demodi <junethir3y@gmail.com>
2017-03-30 16:49:02 +0800

    '修改ajax路径'

---
+++ dist/404.php
@@ -0,0 +1,33 @@
+<?php include('siteconfig.php'); ?>
+<!doctype html>
+<html>
+    <head>
+      <meta charset="utf-8"/>
+	  <meta name="viewport" content="width=device-width">
+	  <title>404 Page Not Found - <?php echo $site_title; ?></title>
+	  <meta name="robots" content="noindex, nofollow">
+      <link href="/style.css" rel="stylesheet" type="text/css">
+      <link href="/fonts/font.css" rel="stylesheet" type="text/css" cache="false" />
+      <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon">
+	  <?php include 'ads/head_code.php'; ?>
+    </head>
+<body>
+<?php include 'includes/header.php'; ?>
+
+<div class="box-pagecontent">
+<div class="wrap">
+<div class="posttitle">
+<h1>Page Not Found</h1>
+</div>
+
+<div class="randombox">
+<p>Page not found. We're sorry, but we couldn't find the page you requested. Please try again or use our site search.
+</p>
+</div>
+
+</div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
+</body>
+</html>
